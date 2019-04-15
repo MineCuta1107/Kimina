61c2262f0221f26506a414b143993c59c720e2b1
DenBagus <45093767+tewgaming@users.noreply.github.com>
2019-04-15 09:03:42 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,29 +1,112 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js") 
 
 module.exports.run = async (bot, message, args) => {
-  const embed = new Discord.RichEmbed()
-  .setTitle("==========>**Commands List**<==========")
-  
-  .setColor("RANDOM")
-  .setDescription("Hi,My Name is Kimina Kekasih. My prefix is `/`")
-  .setFooter("©Kimina Kekasih 2019")
-  
-  .setTimestamp()
-  .addField(":earth_africa: General", '`help` | `ping` | `stats` | `invite`')
-  .addField(":musical_note: Streaming",  '`helpstream`')
-  .addField(":tools: Moderation", '`ban`| `kick`')
-  .addField("Kimina Kekasih Support"," [Invite Me](https://bit.ly/2HU7eTH) | [Vote Me](https://bit.ly/2TZfmJR) | [Support Server](https://bit.ly/2Fsqczu) | [Website](https://bit.ly/2CCGALS)")
+  let pages = [`
+ Hai, My Name is Kimina Kekasih.
+I am bot streaming, on 24/7
 
+ By <@407106326045655050>
+`, `
+ Kimina Kekasih's Command List
+Core Commands
+
+•help
+•invite
+•kick
+•ping
+•stast
+
+
+`, `
+ Kimina Kekasih's Command List
+Play Commands
+
+|LIST RADIO |
+•**Kpop** | **Jpop** | **rfm** | **fun** | **virgin**•
+
+•Usage : /play [list]
+•Example : /play Kpop
+
+`, `
+Kimina Kekasih's Command List
+Stream Commands
+
+|LIST STREAM|
+•**Nightcore** | **Ncs** | **RDI** | **Top Songs**•
+
+•Usage : /stream [list]
+•Example : /stream Nightcore
+
+`, `
+Kimina Kekasih's Support
+**Support Kimina Kekasih**
+
+**[Invite Me](https://bit.ly/2HU7eTH)**
+**[Support Server](https://bit.ly/2Fsqczu)**
+**[Website](https://bit.ly/minekimina)**
+**[Vote Kimina Kekasih on DBL](https://discordbots.org/bot/543013335210393600/vote)**
+**[Invite Mine Cuta Support](https://bit.ly/2OqNto6)**
+
+**©NewStars Trix Development**
+`]
+let page = 1;
   
-  message.channel.send(embed)
+  const embed = new Discord.RichEmbed() 
+    .setThumbnail(bot.user.displayAvatarURL)
+    .setColor(0xff2f2f) 
+    .setAuthor("Help commands list", bot.user.displayAvatarURL)
+    .setFooter(`Page ${page} of ${pages.length} • Kimina Kekasih`)
+    .setDescription(pages[page-1])
+ 
+  message.channel.send(embed).then(msg => {
+   
+    msg.react('⏪').then( r => {
+          msg.react(`❎`).then( r => { 
+      msg.react('⏩')
+     
+      // Filters - These make sure the variables are correct before running a part of code
+      const backwardsFilter = (reaction, user) => reaction.emoji.name === '⏪' && user.id === message.author.id;
+      const delFilter = (reaction, user) => reaction.emoji.name === `❎` && user.id === message.author.id;
+      const forwardsFilter = (reaction, user) => reaction.emoji.name === '⏩' && user.id === message.author.id;
+     
+      const backwards = msg.createReactionCollector(backwardsFilter, { time: 60000 }); 
+      const forwards = msg.createReactionCollector(forwardsFilter, { time: 60000 });
+      const dels = msg.createReactionCollector(delFilter, { time:100000 });
+
+      
+      backwards.on('collect', r => { 
+        if (page === 1) return; 
+        page--;
+	embed.setAuthor("Help commands list", bot.user.displayAvatarURL)
+        embed.setDescription(pages[page-1])
+        embed.setFooter(`Page ${page} of ${pages.length} • Kimina Kekasih`)
+        embed.setThumbnail(bot.user.displayAvatarURL);
+        msg.edit(embed) 
+      })
+     
+      forwards.on('collect', r => {
+        if (page === pages.length) return; 
+        page++;
+	embed.setAuthor("Help commands list", bot.user.displayAvatarURL)
+          embed.setThumbnail(bot.user.displayAvatarURL)
+        embed.setDescription(pages[page-1]);
+        embed.setFooter(`Page ${page} of ${pages.length} • Kimina Kekasih`);
+        msg.edit(embed) 
+   
+    })
+          dels.on('collect', r => {
+          msg.delete(1000)
+        })
+ 
+  }) 
+ 
+})
+  
+})
+
 }
 
-exports.conf = {
-    aliases: ['h'],
-    cooldown: "2"
+exports.help = {
+ name: "help",
+  aliases: "h"
 }
-  module.exports.help = {
-    name: "help",
-    description: 'List Help',
-  usage: 'help'
-  }
