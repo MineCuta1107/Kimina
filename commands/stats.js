b970b623cc4aa3413b4f9df55e583251d75be28e
DenBagus <45093767+tewgaming@users.noreply.github.com>
2019-08-02 20:44:55 +0700

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -9,28 +9,29 @@ let os = require('os')
 let cpuStat = require("cpu-stat")
 const ms = require("ms")//dah
 
-exports.run = (bot, message, args) => {
+exports.run = (client, message, args) => {
     let cpuLol;
     cpuStat.usagePercent(function(err, percent, seconds) {
         if (err) {
             return console.log(err);
         }
-        const duration = moment.duration(bot.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
+        const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
         const embedStats = new Discord.RichEmbed()
             .setAuthor("Kimina Kekasih Stats")
             .setTitle("**BOT Stats**")
             .setColor("RANDOM")
-            .addField(":floppy_disk: > Mem Usage", `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB`, true)
-            .addField(":clock1: > Uptime ", `${duration}`, true)
-            .addField(":busts_in_silhouette: > Users", `${bot.users.size.toLocaleString()}`, true)
-            .addField(":satellite: > Servers", `${bot.guilds.size.toLocaleString()}`, true)
-            .addField(":pencil: > Channels ", `${bot.channels.size.toLocaleString()}`, true)
-            .addField(":file_folder: > Discord.js", `v${version}`, true)
-            .addField(":chains:  > Node", `${process.version}`, true)
-            .addField(":ballot_box_with_check:  > CPU", `\`\`\`md\n${os.cpus().map(i => `${i.model}`)[0]}\`\`\``)
-            .addField(":computer: > CPU usage", `\`${percent.toFixed(2)}%\``, true)
-            .addField(":desktop: > Arch", `\`${os.arch()}\``, true)
-            .addField(":printer: > Platform", `\`\`${os.platform()}\`\``, true)
+//            .addField(`<:6445_ModWarning:551796071383302146> Connected to **shard ${client.shard.id+1}/${client.shard.count}**`)
+            .addField("> Mem Usage", `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB`, true)
+            .addField("> Uptime Bot ", `${duration}`, true)
+            .addField("> Users", `${client.users.size.toLocaleString()}`, true)
+            .addField("> Servers", `${client.guilds.size.toLocaleString()}`, true)
+            .addField("> Channels ", `${client.channels.size.toLocaleString()}`, true)
+            .addField("> Eris", `v0.10.1`)
+            .addField("> Node", `${process.version}`, true)
+            .addField("> CPU", `\`\`\`md\n${os.cpus().map(i => `${i.model}`)[0]}\`\`\``)
+            .addField("> CPU usage", `\`${percent.toFixed(2)}%\``, true)
+            .addField("> Arch", `\`${os.arch()}\``, true)
+            .addField("> Platform", `\`\`${os.platform()}\`\``, true)
         message.channel.send(embedStats) //Thank for helping guys
     }); 
 };
@@ -40,4 +41,4 @@ exports.help = {
    name: "stats",
    description:"stats bots",
     usage: "stats"
-};   
+}; 
